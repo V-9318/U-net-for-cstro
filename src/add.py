c4c5fbb48de5b1315a6100c0d81c2831fe98ad7a
Shang <879202151@qq.com>
2019-10-08 16:30:50 +0800

    初始化仓库

--- src/add.py
+++ src/add.py
@@ -1,1 +1,8 @@
-import
+import os
+import SimpleITK as stk
+
+path = '../build/testdata'
+for name in os.listdir(path):
+    data_nii = stk.ReadImage(os.path.join('../build/testresult/{}_true_data.nii'.format(name)))
+    label_true_nii = stk.ReadImage(os.path.join('../build/testresult/{}_true_label.nii'.format(name)))
+    label_test_nii = stk.ReadImage(os.path.join('../'))
